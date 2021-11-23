e8614b3e3e5a857a97800708633d6073527b9f97
Josue0117 <josuehr000@gmail.com>
2021-11-22 20:57:15 -0600

    Creates unit test for CardsContent component

--- test/CardsContent.spec.js
+++ test/CardsContent.spec.js
@@ -1,34 +1,44 @@
-import { mount, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import CardsContent from '@/components/CardsContent.vue'
+import Paragraph from '@/components/Paragraph.vue'
 
 describe('CardsContent', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(CardsContent)
+    const wrapper = shallowMount(CardsContent)
     expect(wrapper).toBeTruthy()
   })
 
   test('is rendering the first card if cardNumber is equal to 1', () => {
-    const wrapper = mount(CardsContent, {
+    const wrapper = shallowMount(CardsContent, {
       propsData: {
         cardNumber: 1
+      },
+      stubs: {
+        Paragraph: true
       }
     })
     expect(wrapper.find(".card-1").exists()).toBe(true)  
   })
 
   test('is rendering the second card if cardNumber is equal to 2', () => {
-    const wrapper = mount(CardsContent, {
+    const wrapper = shallowMount(CardsContent, {
       propsData: {
         cardNumber: 2
+      },
+      stubs: {
+        Paragraph: true
       }
     })
     expect(wrapper.find(".card-2").exists()).toBe(true)  
   })
 
   test('is rendering the thirt card if cardNumber is equal to 3', () => {
-    const wrapper = mount(CardsContent, {
+    const wrapper = shallowMount(CardsContent, {
       propsData: {
         cardNumber: 3
+      },
+      stubs: {
+        Paragraph: true
       }
     })
     expect(wrapper.find(".card-3").exists()).toBe(true)  
